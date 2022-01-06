540c9a27d65569bc8db4540bc59a391d166d6d16
seth <seth623@gmail.com>
2022-01-06 15:15:18 -0700

    Completed Assignment

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,16 @@
 
 def key_for_min_value(name_hash)
 
+    lowest_key = nil 
+    lowest_value = Float::INFINITY  
+
+    name_hash.each do |key, value|
+        if value < lowest_value 
+            lowest_value = value 
+            lowest_key = key  
+        end
+    end 
+    
+    return lowest_key 
+        
 end
